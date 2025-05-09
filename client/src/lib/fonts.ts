5e1029436f4474ef229e179256e86701cc8ec0ef
dotolink <ggamsire@gmail.com>
2025-05-09 21:56:24 +0900

    폰트 적용
    퐅느 적용

---
+++ client/src/lib/fonts.ts
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+
+const getPaperlogyStyle = (weight: 'thin' | 'extralight' | 'light' | 'regular' | 'medium' | 'semibold' | 'bold' | 'extrabold' | 'black') => {
+  const weightMap = {
+    thin: 'font-paperlogy-thin',
+    extralight: 'font-paperlogy-extralight',
+    light: 'font-paperlogy-light',
+    regular: 'font-paperlogy-regular',
+    medium: 'font-paperlogy-medium',
+    semibold: 'font-paperlogy-semibold',
+    bold: 'font-paperlogy-bold',
+    extrabold: 'font-paperlogy-extrabold',
+    black: 'font-paperlogy-black'
+  };
+
+  return weightMap[weight];
+};
+
+export { getPaperlogyStyle }; 

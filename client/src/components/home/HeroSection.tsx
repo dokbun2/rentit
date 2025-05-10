ca9776d8aa5d3d89ee3043c01cda24247c547e31
dotolink <ggamsire@gmail.com>
2025-05-11 01:54:33 +0900

    1
    1

--- client/src/components/home/HeroSection.tsx
+++ client/src/components/home/HeroSection.tsx
@@ -41,11 +41,11 @@ const HeroSection = () => {
             className="md:w-1/2 md:pr-8"
           >
             <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight mb-4">
-              <span className="text-white">렌탈 비즈니스의 </span>
-              <span className="text-primary">시작</span>
+              <span className="text-primarywhite">렌탈 비즈니스 </span>
+              <span className="text-white">모든 것!</span>
               <br />
-              <span className="text-white">렌잇에서</span>
-              <span className="text-primary">도와 드리겠습니다.</span>
+              <span className="text-white">렌잇과 </span>
+              <span className="text-primary">함께 해요!</span>
             </h1>
             
             <motion.p 

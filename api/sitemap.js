94aa8a61e8e500df3fc9a495a9efb9fc20dbceb8
dotolink <ggamsire@gmail.com>
2025-05-13 11:07:05 +0900

    Fix: sitemap.xml 제공 방식 개선

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,31 +1,35 @@
 export default function handler(req, res) {
   // XML 형식으로 응답 설정
   res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+  res.setHeader('Cache-Control', 'public, max-age=86400');
+  
+  // 현재 날짜를 YYYY-MM-DD 형식으로 가져오기
+  const today = new Date().toISOString().split('T')[0];
   
   // sitemap XML 내용
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://renit.dokbun2.com/</loc>
-    <lastmod>2023-10-01</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
     <loc>https://renit.dokbun2.com/about</loc>
-    <lastmod>2023-10-01</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
     <loc>https://renit.dokbun2.com/services</loc>
-    <lastmod>2023-10-01</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
     <loc>https://renit.dokbun2.com/contact</loc>
-    <lastmod>2023-10-01</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
   </url>

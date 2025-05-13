d1acb03abe4fe93aa1ffac2fcea9e6fa77801498
dotolink <ggamsire@gmail.com>
2025-05-14 06:23:23 +0900

    지친다 끈내자
    ㅁ

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,12 +7,22 @@ import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
 import NewsManagement from "./pages/NewsManagement";
 import RentalNews from "./pages/RentalNews";
+import { useEffect } from "react";
+
+// 어드민 경로 접근 시 처리를 위한 중간 라우트
+const AdminRedirect = () => {
+  useEffect(() => {
+    window.location.href = "/admin/dashboard";
+  }, []);
+  return null;
+};
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={HomePage} />
       <Route path="/news" component={RentalNews} />
+      <Route path="/admin" component={AdminRedirect} />
       <Route path="/admin/login" component={AdminLogin} />
       <Route path="/adminlogin" component={AdminLogin} />
       <Route path="/admin/dashboard" component={AdminDashboard} />
